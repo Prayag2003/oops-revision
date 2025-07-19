f467bb7d0802c2b97b2988cfc6fe569825c421de
Prayag Bhatt <prayagbhatt2003@gmail.com>
2025-07-19 19:24:36 +0530

    [feat-packages]: add Greeting and Message classes with basic functionality

---
+++ src/com/google/packages/Greeting/Greeting.java
@@ -0,0 +1,12 @@
+package com.google.packages.Greeting;
+
+import static com.google.packages.Message.Message.message;
+
+public class Greeting {
+    public static void main(String[] args) {
+        // Packages are just a way to organize classes. They are used to group related classes together.
+        // creates compartments and folders for our classes.
+        System.out.println("Hello, World!");
+        message();
+    }
+}
---
+++ src/com/google/packages/Message/Message.java
@@ -0,0 +1,11 @@
+package com.google.packages.Message;
+
+public class Message {
+    public static void main(String[] args) {
+        System.out.println("Hello from the Message package!");
+    }
+
+    public static void message(){
+        System.out.println("This is a message from the Message class.");
+    }
+}
