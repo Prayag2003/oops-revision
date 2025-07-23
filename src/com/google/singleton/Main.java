ae0b61e8963977a612ded9c62fdd2b2f404e0998
Prayag Bhatt <prayagbhatt2003@gmail.com>
2025-07-23 20:31:17 +0530

    feat: add comment to clarify Singleton instance behavior in Main class

--- src/com/google/singleton/Main.java
+++ src/com/google/singleton/Main.java
@@ -2,6 +2,7 @@ package com.google.singleton;
 
 public class Main{
     public static void main(String[] args) {
+        // all objects point to the same instance
         Singleton obj1 = Singleton.getInstance();
         Singleton obj2 = Singleton.getInstance();
         System.err.println(obj1.toString());
