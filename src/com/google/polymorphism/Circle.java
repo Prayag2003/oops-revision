cd5b7a1664485ecbfb6e25100817454fdcbcad0e
Prayag Bhatt <prayagbhatt2003@gmail.com>
2025-07-27 16:07:30 +0530

    feat: static can be inherited but cant be overridden

--- src/com/google/polymorphism/Circle.java
+++ src/com/google/polymorphism/Circle.java
@@ -5,4 +5,27 @@ public class Circle extends Shapes {
     void area() {
         System.out.println("Area of Circle is πr²");
     }
+
+    // @Override
+    // Method Hiding
+    // This is not an override, it's method hiding because static methods are not
+    // polymorphic.
+    static void staticMethod() {
+        System.out.println("This is a static method in the Circle class!");
+    }
+
+    public static void main(String[] args) {
+        Shapes.staticMethod();
+        Shapes circle = new Circle();
+        // This will call the static method from Shapes, not Circle, why?
+        // Because static methods are resolved at compile time based on the reference
+        // type.
+        circle.staticMethod();
+
+        Circle c = new Circle();
+        // this will call the static method from Circle, why?
+        // Because static methods are resolved at compile time based on the reference
+        // type.
+        c.staticMethod();
+    }
 }
