eca5fb30449a081904607c53d2496fe3dec5f223
Prayag Bhatt <prayagbhatt2003@gmail.com>
2025-07-27 18:36:46 +0530

    feat: encapsulation and abstraction

---
+++ src/com/google/encapsulation/Main.java
@@ -0,0 +1,24 @@
+package com.google.encapsulation;
+
+public class Main {
+    // NOTE: Encapsulation is the wrapping of data and methods into a single unit,
+    // i.e, class
+    // It's like solving an implemenetation level issue
+    // Process of containing information
+
+    // NOTE: Abstraction is different from encapsulation, since it is the hiding the
+    // unnecessary details and showing valuable information only.
+    // In order to start a Car, do you know to understand how the engine works? Or
+    // what happens when you key the ignition?
+    // No, you just need to know how to start it. This is abstraction.
+    // It's like solving a design level issue
+    // Process of gaining information
+
+    // NOTE: Data Hiiding is the process of restricting access to certain details of
+    // an
+    // object.
+    // Encapsulation is a way to achieve data hiding.
+    // like a private variable in a class, which is not accessible outside the
+    // class.
+
+}
