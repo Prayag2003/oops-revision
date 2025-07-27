8cf2f43a62597871c18b06af4ec26e5494cb86c9
Prayag Bhatt <prayagbhatt2003@gmail.com>
2025-07-27 16:07:08 +0530

    feat: static methods can't be overridden

--- src/com/google/polymorphism/Shapes.java
+++ src/com/google/polymorphism/Shapes.java
@@ -1,13 +1,19 @@
 package com.google.polymorphism;
 
 public class Shapes {
-    // NOTE:
-    // cannot override a final method
+    // NOTE: Final methods cannot be overridden, thus prevents overriding.
     // final void area() {
     // System.out.println("This is the shape class!");
     // }
-
     void area() {
         System.out.println("This is the shape class!");
+    }
+
+    // NOTE: If we declare a class as final, it prevents inheritance as well as all
+    // the methods of that class become implicitly final.
+
+    // NOTE: Static methods cannot be overridden, but can be used in subclasses.
+    static void staticMethod() {
+        System.out.println("This is a static method in the Shape class!");
     }
 }
