1da7e5e83357b3e32cdbc42ab60941a10709da69
Prayag Bhatt <prayagbhatt2003@gmail.com>
2025-07-27 15:36:49 +0530

    feat: final method can't be overridden

--- src/com/google/polymorphism/Shapes.java
+++ src/com/google/polymorphism/Shapes.java
@@ -1,6 +1,12 @@
 package com.google.polymorphism;
 
 public class Shapes {
+    // NOTE:
+    // cannot override a final method
+    // final void area() {
+    // System.out.println("This is the shape class!");
+    // }
+
     void area() {
         System.out.println("This is the shape class!");
     }
