4f23652c9d31d0c75d1ccc1d7e9dd5d9db7b9ecd
Prayag Bhatt <prayagbhatt2003@gmail.com>
2025-07-30 22:30:56 +0530

    feat: interface extends interface

---
+++ src/com/google/interfaces/extendsdemo/A.java
@@ -0,0 +1,5 @@
+package com.google.interfaces.extendsdemo;
+
+public interface A {
+    void fun();
+}
---
+++ src/com/google/interfaces/extendsdemo/B.java
@@ -0,0 +1,5 @@
+package com.google.interfaces.extendsdemo;
+
+public interface B extends A {
+    void greet();
+}
